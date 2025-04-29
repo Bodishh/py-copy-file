8a248255a30a04e0516035f27fa2719db231ae28
Bohdan <bsuhij@gmail.com>
2025-04-29 17:58:55 +0300

    Made changes

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 def copy_file(command: str) -> None:
-    comand_list = command.split()
+    command_list = command.split()
 
-    if len(comand_list) != 3:
+    if len(command_list) != 3:
         return
 
-    if comand_list[0] == "cp":
-        if comand_list[1] != comand_list[2]:
+    if command_list[0] == "cp":
+        if command_list[1] != command_list[2]:
             try:
-                with open(comand_list[1], "r") as file_in, \
-                     open(comand_list[2], "w") as file_out:
+                with (
+                    open(command_list[1], "r") as file_in,
+                    open(command_list[2], "w") as file_out
+                ):
                     need_to_write = file_in.read()
                     file_out.write(need_to_write)
             except FileNotFoundError:
